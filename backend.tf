5e17fadc52d2113f9a7764a967559ef1c81d36c5
avpaws444 <104503706+avpaws444@users.noreply.github.com>
2023-12-20 18:54:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,16 +1,4 @@
-resource "aws_s3_bucket" "backendbucket" {
-   bucket = "terra-cloud-jenkins-ashok"
-}
 
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "sseconfig" {
-      bucket = aws_s3_bucket.backendbucket.bucket
-      rule {
-     apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-           }
-      }
-}
 terraform {
   backend s3{
     bucket = "avpaws444"

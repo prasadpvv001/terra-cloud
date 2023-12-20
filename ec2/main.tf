20e47626c7b9c4c0718370028ecff77c33efc36e
avpaws444 <104503706+avpaws444@users.noreply.github.com>
2023-12-20 20:50:23 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "webserver_access" {
 
 resource "aws_instance" "ourfirst" {
   ami           = "ami-0fc5d935ebf8bc3bc"
-  region = "us-east-1"
+  availability_zone = "ap-south-1a"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.webserver_access.name}"]
   
